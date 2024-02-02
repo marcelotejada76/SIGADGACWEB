c857cbb7937f47ac44960591d4bc99acfa4729de
marcelotejada76 <marcelotejada@gmail.com>
2024-02-02 12:29:26 -0500

    descarga pdf

--- SIGADGACWEB/SistemaIntegradoGestion/Controllers/ConsultaFr3Controller.cs
+++ SIGADGACWEB/SistemaIntegradoGestion/Controllers/ConsultaFr3Controller.cs
@@ -2,7 +2,10 @@
 using CapaModelo;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -81,6 +84,9 @@ namespace SistemaIntegradoGestion.Controllers
             {
                 if (NumeroFr3 > 0)
                 {
+                   
+
+
                     DetalleFr3 = CD_Fr3.Instancia.ConsultaFr3PorSecuencia(NumeroFr3, Ato,Ano);
 
                     return Json(DetalleFr3, JsonRequestBehavior.AllowGet);
@@ -95,7 +101,71 @@ namespace SistemaIntegradoGestion.Controllers
             }
 
         }
-      
-      
+
+       
+        //public JsonResult DescargaFr3(Int16 NumeroFr3, string Ato, string Ano, string FechaEmision, string Ruc)
+            public ActionResult DescargaFr3(Int16 NumeroFr3, string Ato,  string FechaEmision, string Ruc)
+        {
+
+            //string miDirectorio = @"c:\Fr3Pdf";
+            //if (!Directory.Exists(miDirectorio))
+            //    Directory.CreateDirectory(miDirectorio);
+
+
+            string remoteUri = @"\\172.20.19.55\PdfFr3\fr3No" + NumeroFr3 + "fecha" + FechaEmision + "cliente" + Ruc + "Ato" + Ato + ".pdf";
+            string fileName = "fr3No"+NumeroFr3+"fecha"+FechaEmision+"cliente"+Ruc+"Ato"+ Ato + ".pdf";
+
+            byte[] fileBytes = GetFile(remoteUri);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+
+            //string rutadescarga = @"c:\Fr3Pdf\" + fileName;
+            //// Create a new WebClient instance.
+            //WebClient myWebClient = new WebClient();
+            //// Concatenate the domain with the Web resource filename.
+            //myStringWebResource = remoteUri + fileName;
+            //Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+            //// Download the Web resource and save it into the current filesystem folder.
+            //myWebClient.DownloadFile(myStringWebResource, rutadescarga);
+
+            //return null;
+
+
+
+
+
+            //tbFr3 DetalleFr3 = new tbFr3();
+
+            //if (Session["Usuario"] == null)
+            //    return Json(DetalleFr3, JsonRequestBehavior.AllowGet);
+
+            //try
+            //{
+            //    if (NumeroFr3 > 0)
+            //    {
+            //        DetalleFr3 = CD_Fr3.Instancia.ConsultaFr3PorSecuencia(NumeroFr3, Ato, Ano);
+
+            //        return Json(DetalleFr3, JsonRequestBehavior.AllowGet);
+            //    }
+            //    else
+            //        return Json(DetalleFr3, JsonRequestBehavior.AllowGet);
+            //}
+            //catch (Exception ex)
+            //{
+            //    return Json(DetalleFr3, JsonRequestBehavior.AllowGet);
+            //    throw ex;
+            //}
+
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
     }
 }
